f7c377540eba438afd4d2748a5cefff49cf6fc76
Brandon <brandon.redford@outlook.com>
2024-07-05 16:13:43 -0700

    create server for backend

---
+++ capstone_server/routes/auth.js
@@ -0,0 +1,100 @@
+require("dotenv").config();
+
+const knex = require("knex")(require("../knexfile"));
+const express = require("express");
+const router = express.Router();
+
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const { ACCESS_TOKEN_SECRET, JWT_EXPIRY } = process.env
+
+// Login
+router.post("/login", async (req, res) => {
+
+    const { username, password} = req.body
+
+    if (!username) {
+        return res.status(400).json({
+            message: 'Username required',
+        });
+    } else if (!password) {
+        return res.status(400).json({
+            message: 'Password required',
+        });
+    }
+
+    try {
+
+        const user = await knex('users').where({ username }).first();
+        if (!user) {
+            return res.status(401).json({ message: 'User not found' })
+        }
+
+        const validPassword = await bcrypt.compare( password, user.password )
+        if (!validPassword) {
+            return res.status(401).json({ message: 'Invalid password' });
+        }
+
+        const token = jwt.sign({ username: user.username }, ACCESS_TOKEN_SECRET, { expiresIn: JWT_EXPIRY });
+
+        return res.status(201).json({
+            success: true,
+            message: 'User Logged In',
+            token,
+        });
+
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({
+          message: 'Server-side Error',
+        });
+    }
+});
+
+// Register
+router.post("/register", async (req, res) => {
+
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+        return res.status(400).json({
+            message: 'Please ensure all fields are entered in request body'
+        })
+    }
+
+    try {
+        const userExists = await knex('users').where({ email }).first();
+
+        if (userExists) {
+            return res.status(409).json({
+                message: `User with email ${email} already exists`
+            })
+        }
+
+        const hashPassword = await bcrypt.hash(password, 10)
+
+        await knex('users').insert({
+            username,
+            email,
+            password: hashPassword,
+        })
+
+        const newUser = await knex('users').where({ email }).first();
+
+        const token = jwt.sign({ email: newUser.email }, ACCESS_TOKEN_SECRET, { expiresIn: JWT_EXPIRY });
+
+        return res.status(201).json({
+            success: true,
+            message: 'Registered New User',
+            token,
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({
+          message: 'Server-side Error',
+        });
+    }
+})
+
+
+module.exports = router;
--- capstone_server/routes/exercises.js
+++ capstone_server/routes/exercises.js
@@ -2,15 +2,102 @@ const knex = require("knex")(require("../knexfile"));
 const express = require("express");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus(401)
+
+    JsonWebTokenError.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      if (err) return res.sendStatus(403)
+      req.user = user
+      next()
+    })
+}
+
+// Exercises ENDPOINTS
+router.get("/", authenticateToken, async (req, res) => {
     try {
         const exercises = await knex('exercises');
-        res.json(exercises);
-    } catch {
-        res.status(400).json({
-            message: 'Error gettting exercises'
-        })
+        if (exercises) {
+            res.status(200).json(exercises);
+        } else {
+            res.status(400).json({
+                message: 'Error gettting exercises'
+            })
+        } 
+    } catch (err) {
+            res.status(500).send(`Error retrieving exercises: ${err}`);
     }
 });
+
+router.get("/:id", authenticateToken, async (req, res) => {
+    try {
+        const exercises = await knex('exercises');
+        const exerciseId = req.params.id;
+        const exercise = exercises.find((exercise) => exercise.exercise_id == exerciseId);
+        if (exercise) {
+            res.json(exercise);
+        } else {
+            res.status(404).json(`Exercise id: ${exerciseId} does not exist`);
+        }
+    } catch (err) {
+        res.status(500).send(`Error retrieving exercise: ${err}`);
+    }
+});
+
+// Comments ENDPOINTS
+router.get("/:id/comments", authenticateToken, async (req, res) => {
+    try {
+        const commentsTable = await knex('comments');
+        const exerciseId = req.params.id;
+        const allComments = commentsTable.find((allComments) => allComments.exercise_id == exerciseId);
+        if (allComments) {
+            res.status(200).json(allComments);
+        } else {
+            res.status(404).json(`Comments under exercise id: ${exerciseId} not found`);
+        }
+    } catch (err) {
+            res.status(500).send(`Error retrieving comments: ${err}`);
+    }
+});
+
+router.post("/:id/comments", authenticateToken, async (req, res) => {
+    try {
+        const commentsTable = await knex('comments');        
+
+        const {
+            exercise_id,
+            user_id,
+            comment_text
+        } = req.body;
+
+        const requiredProps = [
+            "exercise_id",
+            "user_id",
+            "comment_text"
+        ];
+
+        const missingProps = requiredProps.filter
+        ((prop) => !req.body.hasOwnProperty(prop));
+        if (missingProps.length > 0) {
+            return res
+                .status(400)
+                .json({ error: `Missing properties: ${missingProps.join(", ")}` })
+        }
+
+        insertedComment = commentsTable.insert({
+            exercise_id,
+            user_id,
+            comment_text
+        });
+
+        res.status(201).json({ message: "Comment posted successfully", comment: insertedComment });
+
+    } catch (err) {
+        res.status(500).send(`Error posting comment: ${err}`);
+    }
+});
+
+
 
 module.exports = router;
